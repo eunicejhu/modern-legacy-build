c96b5132c3596d6da4bae91b68ffa797861486e3
Isabella Hu <eunicejhu@gmail.com>
2020-12-10 02:15:28 +0800

    clean: deprecated code
    Signed-off-by: Isabella Hu <eunicejhu@gmail.com>

--- src/math/index.js
+++
@@ -1,9 +0,0 @@
-import { Arithmatic } from "./logger";
-const add = (x, y) => x + y;
-const substract = (x, y) => x - y;
-const delayedMultiply = (x, y) =>
-    new Promise((resolve) => {
-        setTimeout(resolve(x * y), 1000);
-    });
-const multiply = (x, y) => Arithmatic.multiply(x, y);
-export { add, substract, delayedMultiply, multiply };
