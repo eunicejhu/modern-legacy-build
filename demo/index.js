d086700c32b018636df791054672a3fc7fbf0b01
Isabella Hu <eunicejhu@gmail.com>
2020-12-09 15:05:03 +0800

    feat: test umd in node
    Signed-off-by: Isabella Hu <eunicejhu@gmail.com>

--- demo/index.js
+++ demo/index.js
@@ -1,3 +1,3 @@
-const { add, multiply } = require("modern-legacy-build/lib/math/index");
+const { add, multiply } = require("modern-legacy-build/dist/index.umd");
 console.log(add(1, 2));
 console.log(multiply(2, 3));
