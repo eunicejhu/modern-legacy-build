67535f8314cf338cbd71fface4e0d60dae0fe642
Isabella Hu <eunicejhu@gmail.com>
2020-12-09 14:28:38 +0800

    feat: test cjs build in node
    Signed-off-by: Isabella Hu <eunicejhu@gmail.com>

--- demo/index.js
+++ demo/index.js
@@ -1,4 +1,3 @@
-// import { add, multiply } from "modern-legacy-build";
-const { add, multiply } = require("modern-legacy-build");
+const { add, multiply } = require("../dist/index");
 console.log(add(1, 2));
 console.log(multiply(2, 3));
