a13ce2d5b2d37d2cd194136aa828b5485d4245ae
Isabella Hu <eunicejhu@gmail.com>
2020-12-15 23:22:43 +0800

    feat: add some helpers & test
    Signed-off-by: Isabella Hu <eunicejhu@gmail.com>

--- src/math/helper.js
+++ src/math/helper.js
@@ -1,3 +1,47 @@
 export const capitalize = (str) => {
     return str.charAt(0).toUpperCase() + str.substr(1);
 };
+
+export const chainAlerts = () => {
+    return new Promise((resolve) => resolve(2));
+};
+
+export const cloneDeep = (obj) => {
+    return JSON.parse(JSON.stringify(obj));
+};
+
+export const sum = function (a) {
+    return function (b) {
+        return a + b;
+    };
+};
+
+export const printNumbers1 = function (from, to) {
+    let delay = 1000;
+    let count = from;
+    // option1: setInterval
+    print(); // print immediately
+    const intervalId = setInterval(print, delay);
+    function print() {
+        console.log(count);
+        if (count < to) {
+            count++;
+        } else {
+            clearInterval(intervalId);
+        }
+    }
+};
+
+export const printNumbers2 = function (from, to) {
+    let delay = 1000;
+    let count = from;
+    // option 2: setTimout
+    setTimeout(print, delay);
+    function print() {
+        console.log(count);
+        if (count < to) {
+            count++;
+            setTimeout(print, delay);
+        }
+    }
+};
--- test/helper.spec.js
+++ test/helper.spec.js
@@ -1,5 +1,58 @@
-import { capitalize } from "../src/math/helper";
+import {
+    capitalize,
+    chainAlerts,
+    cloneDeep,
+    sum,
+    printNumbers1,
+    printNumbers2,
+} from "../src/math/helper";
 
-it.only("captalize", () => {
+it("captalize", () => {
     expect(capitalize("hello")).toBe("Hello");
+});
+
+it("chainAlerts", () => {
+    chainAlerts()
+        .then((result) => {
+            expect(result).toBe(2);
+            return Promise.reject("whoops");
+        })
+        .catch((error) => {
+            console.log({ error });
+            expect(error).toBe("something");
+        })
+        .finally((data) => {
+            console.log({ data });
+            expect(data).toBeUndefined();
+        });
+});
+
+it("cloneDeep", () => {
+    const obj = { id: 1, next: { id: 2, value: 3 } };
+    const copy = cloneDeep(obj);
+
+    obj.next.value = 4;
+    expect(copy.next.value).toBe(3);
+});
+
+it("sum", () => {
+    expect(sum(2)(3)).toBe(5);
+});
+
+it("printNumbers1", (done) => {
+    jest.spyOn(global.console, "log");
+    printNumbers1(2, 5);
+    setTimeout(() => {
+        expect(console.log).toHaveBeenCalledTimes(4);
+        done();
+    }, 5000);
+});
+
+it.only("printNumbers2", (done) => {
+    jest.spyOn(global.console, "log");
+    printNumbers2(2, 5);
+    setTimeout(() => {
+        expect(console.log).toHaveBeenCalledTimes(4);
+        done();
+    }, 5000);
 });
