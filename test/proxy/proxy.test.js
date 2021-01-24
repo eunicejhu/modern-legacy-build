a48a3985811e7256027ed7e4de20a28bd5fd9207
Isabella Hu <eunicejhu@gmail.com>
2021-01-24 23:14:03 +0100

    test: add makeObservable test
    Signed-off-by: Isabella Hu <eunicejhu@gmail.com>

--- test/proxy/proxy.test.js
+++ test/proxy/proxy.test.js
@@ -1,4 +1,4 @@
-import { wrap, arrayProxy } from "../../src/proxy/index";
+import { wrap, arrayProxy, makeObservable } from "../../src/proxy/index";
 
 test("wrap", () => {
     let user = {
@@ -18,4 +18,12 @@ test("arrayProxy", () => {
     let proxyArray = arrayProxy(array);
 
     expect(proxyArray[-1]).toBe(3);
+});
+
+test("makeObservable", () => {
+    jest.spyOn(console, "log");
+    let user = {};
+    user = makeObservable(user);
+    user.name = "isa";
+    expect(console.log).toHaveBeenCalledWith("SET name = isa");
 });
